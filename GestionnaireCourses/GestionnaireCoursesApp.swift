bf7787b1e6bdb5c8eccfd6ed1644c5955f2068d6
Nicolas Constantin <nicolasconstantin@hotmail.fr>
2025-03-17 21:38:28 +0100

    Initial Commit

---
+++ GestionnaireCourses/GestionnaireCoursesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestionnaireCoursesApp.swift
+//  GestionnaireCourses
+//
+//  Created by Nicolas Constantin on 17/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct GestionnaireCoursesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GestionnaireCoursesTests/GestionnaireCoursesTests.swift
@@ -0,0 +1,17 @@
+//
+//  GestionnaireCoursesTests.swift
+//  GestionnaireCoursesTests
+//
+//  Created by Nicolas Constantin on 17/03/2025.
+//
+
+import Testing
+@testable import GestionnaireCourses
+
+struct GestionnaireCoursesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
